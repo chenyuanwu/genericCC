6b9a10c544d2f0176009df9e3d1bee1d7424af39
chenyuanwu <1404059145@qq.com>
2020-05-12 21:25:46 +0800

    [patched API]debugging pybind wrapper

---
+++ python-wrapper-pantheon.cc
@@ -0,0 +1,16 @@
+#include <pybind11/pybind11.h>
+#include "copa-sender.hh"
+#include "generic-receiver.hh"
+
+using namespace std;
+namespace py = pybind11;
+
+PYBIND11_MODULE(genericCC, m) {
+    py::class_<COPASender>(m, "COPASender")
+        .def(py::init<string, int, int>())
+        .def("send", &COPASender::send);
+
+    py::class_<Receiver>(m, "Receiver")
+        .def(py::init<int>())
+        .def("recvfrom", &Receiver::recvfrom);
+}
