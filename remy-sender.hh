94b159e47fd01f7845fe9faa6327c7aee18fcc0c
chenyuanwu <1404059145@qq.com>
2020-05-26 17:02:42 +0800

    [patched API]debugging remy

--- remy-sender.hh
+++ remy-sender.hh
@@ -11,63 +11,40 @@
 #include "ctcp.hh"
 
 
-//RemyCC parse_remy_config(string filename) {
-//    WhiskerTree whiskers;
-//    int fd = open( filename.c_str(), O_RDONLY );
-//    if ( fd < 0 ) {
-//        fprintf( stderr, "Error in open %s.\n", filename.c_str() );
-//        perror( "open" );
-//        exit( 1 );
-//    }
-//
-//    RemyBuffers::WhiskerTree tree;
-//    if ( !tree.ParseFromFileDescriptor( fd ) ) {
-//        fprintf( stderr, "Could not parse %s.\n", filename.c_str() );
-//        exit( 1 );
-//    }
-//
-//    whiskers = WhiskerTree( tree );
-//
-//    if ( close( fd ) < 0 ) {
-//        fprintf( stderr, "Error in close %s.\n", filename.c_str() );
-//        perror( "close" );
-//        exit( 1 );
-//    }
-//    RemyCC congctrl( whiskers );
-//    return congctrl;
-//}
+RemyCC parse_remy_config(string filename) {
+    WhiskerTree whiskers;
+    int fd = open( filename.c_str(), O_RDONLY );
+    if ( fd < 0 ) {
+        fprintf( stderr, "Error in open %s.\n", filename.c_str() );
+        perror( "open" );
+        exit( 1 );
+    }
+
+    RemyBuffers::WhiskerTree tree;
+    if ( !tree.ParseFromFileDescriptor( fd ) ) {
+        fprintf( stderr, "Could not parse %s.\n", filename.c_str() );
+        exit( 1 );
+    }
+
+    whiskers = WhiskerTree( tree );
+
+    if ( close( fd ) < 0 ) {
+        fprintf( stderr, "Error in close %s.\n", filename.c_str() );
+        perror( "close" );
+        exit( 1 );
+    }
+    RemyCC congctrl( whiskers );
+    return congctrl;
+}
 
 class REMYSender {
 private:
-    WhiskerTree whiskers;
     CTCP< RemyCC > connection;
     int src_id;
 public:
     REMYSender(string serverip, int serverport, int sourceport)
-            : whiskers(), connection(whiskers, serverip, serverport, sourceport, 1), src_id()
+            : connection(parse_remy_config("RemyCC-2014-100x.dna"), serverip, serverport, sourceport, 1), src_id()
     {
-        std::string filename = "RemyCC-2014-100x.dna";
-        int fd = open( filename.c_str(), O_RDONLY );
-        if ( fd < 0 ) {
-            perror( "open" );
-            exit( 1 );
-        }
-
-        RemyBuffers::WhiskerTree tree;
-        if ( !tree.ParseFromFileDescriptor( fd ) ) {
-            fprintf( stderr, "Could not parse %s.\n", filename.c_str() );
-            exit( 1 );
-        }
-
-        whiskers = WhiskerTree( tree );
-
-        if ( close( fd ) < 0 ) {
-            perror( "close" );
-            exit( 1 );
-        }
-
-        RemyCC remycc( whiskers );
-        connection.congctrl = remycc;
         connection.congctrl_init();
 
         PRNG prng(global_PRNG());
